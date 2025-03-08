f991336ec189574ff9e42fd9274d99270e57b131
Sameer Sayyed <sameer07x19@gmail.com>
2025-03-08 11:57:36 +0530

    update

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -74,23 +74,23 @@ const LoginPage = ({ role }) => {
     };
 
     const guestModeHandler = () => {
-        const password = "zxc"
+        const password = "123"
 
         if (role === "Admin") {
-            const email = "yogendra@12"
+            const email = "adminm@123"
             const fields = { email, password }
             setGuestLoader(true)
             dispatch(loginUser(fields, role))
         }
         else if (role === "Student") {
             const rollNum = "1"
-            const studentName = "Dipesh Awasthi"
+            const studentName = "student1"
             const fields = { rollNum, studentName, password }
             setGuestLoader(true)
             dispatch(loginUser(fields, role))
         }
         else if (role === "Teacher") {
-            const email = "tony@12"
+            const email = "teacher@123"
             const fields = { email, password }
             setGuestLoader(true)
             dispatch(loginUser(fields, role))
